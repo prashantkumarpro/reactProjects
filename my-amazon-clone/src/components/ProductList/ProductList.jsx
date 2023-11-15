dd185575dd10aff3f707ee2b50500aa39c090ee7
Prashant Kumar <programmerkrprashant@gmail.com>
2023-11-15 22:09:56 +0530

    productListing page

--- my-amazon-clone/src/components/ProductList/ProductList.jsx
+++ my-amazon-clone/src/components/ProductList/ProductList.jsx
@@ -8,19 +8,25 @@ export const ProductList = () => {
         { id: 4, name: 'Product 4', price: 15.99 },
         // Add more products as needed
     ];
-  
+
     return (
         <section>
-            <h2>Product List</h2>
-            <ul>
-                {products.map(product => (
-                    <li className='box' key={product.id}>
-                        <h3>{product.name}</h3>
-                        <p>${product.price}</p>
-                        <button>Add to Cart</button>
-                    </li>
-                ))}
-            </ul>
+            <div className="grid">
+                <div className="filter">filter
+                    <div>Like rows, a column track is created for each value specified for grid-template-columns.</div>
+                </div>
+                <div className="product-grid"> <ul>
+                    {products.map(product => (
+                        <li className='box' key={product.id}>
+                            <h3>{product.name}</h3>
+                            <p>${product.price}</p>
+                            <button>Add to Cart</button>
+                        </li>
+                    ))}
+                </ul></div>
+            </div>
+
+
         </section>
     )
 }
