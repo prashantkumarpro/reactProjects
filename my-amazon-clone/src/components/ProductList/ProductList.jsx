b54a85e6417bd7424e3c18af366a041855d298cb
Prashant Kumar <programmerkrprashant@gmail.com>
2023-11-16 22:01:13 +0530

    product listing page

--- my-amazon-clone/src/components/ProductList/ProductList.jsx
+++ my-amazon-clone/src/components/ProductList/ProductList.jsx
@@ -6,6 +6,10 @@ export const ProductList = () => {
         { id: 2, name: 'Product 2', price: 34.99 },
         { id: 3, name: 'Product 3', price: 15.99 },
         { id: 4, name: 'Product 4', price: 15.99 },
+        { id: 5, name: 'Product 5', price: 20.99 },
+        { id: 6, name: 'Product 6', price: 34.99 },
+        { id: 7, name: 'Product 7', price: 15.99 },
+        { id: 8, name: 'Product 8', price: 15.99 },
         // Add more products as needed
     ];
 
