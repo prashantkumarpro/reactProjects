35d31e98459090a8c519458c1b9462315b6b0382
Prashant Kumar <programmerkrprashant@gmail.com>
2023-11-23 16:03:39 +0530

    Single Product page add input for filtering

--- my-amazon-clone/src/components/ProductDetail/SingleProduct.jsx
+++ my-amazon-clone/src/components/ProductDetail/SingleProduct.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { products } from '../ProductList/data'
 import './SingleProduct.css'
+
 
 
 const SingleProduct = () => {
@@ -11,14 +12,29 @@ const SingleProduct = () => {
 
     const [relatedProduct, setRelatedProduct] = useState([])
 
+    const [amount, setAmount] = useState('')
+
+    const handleFilterChange = (e) => {
+        setAmount(e.target.value)
+    }
+
     useEffect(() => {
+
+        // single product
         const filterProduct = products.filter((product) => product.id == id)
         // console.log(filterProduct)
         setProduct(filterProduct[0])
 
+        // filter product by brand name
         const filterBrandName = products.filter((brandName) => brandName.brand === product.brand)
-        console.log(filterBrandName)
+        // console.log(filterBrandName)
         setRelatedProduct(filterBrandName)
+
+        // Filter by amount
+
+        const filterAmount = products.filter((mobile) => amount === '' || mobile.price <= parseInt(amount))
+        console.log(filterAmount)
+        setAmount(filterAmount)
 
     }, [id, product.brand])
 
@@ -43,21 +59,33 @@ const SingleProduct = () => {
                 </div>
             </div>
 
-            <div className="product-grid"><ul>
-                {relatedProduct.map(product => (
-                    <li
-                        className='box'
-                        key={product.id}>
-                        <div className="product-img">
-                            <img src={product.imageUrl} alt="product-image" />
-                        </div>
-                        <h3>{product.name}</h3>
+            <label>
+                Filter by Amount:
+                <input
+                    type="number"
+                    value={amount}
+                    onChange={handleFilterChange}
+                />
+            </label>
 
-                        <p className='price'>₹ {product.price}</p>
-                        <button>Add to Cart</button>
-                    </li >
-                ))}
-            </ul></div>
+            <div className="product-grid">
+                <h2 className='same-brands'>Same Mobiles Brand</h2>
+                <ul>
+                    {relatedProduct.map(product => (
+                        <Link
+                            to={`/SingleProduct/${product.id}`}
+                            className='box'
+                            key={product.id}>
+                            <div className="product-img">
+                                <img src={product.imageUrl} alt="product-image" />
+                            </div>
+                            <h3>{product.name}</h3>
+
+                            <p className='price'>₹ {product.price}</p>
+                            <button>Add to Cart</button>
+                        </Link >
+                    ))}
+                </ul></div>
         </>
 
     )
