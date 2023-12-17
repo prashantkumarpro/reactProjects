78e84830a4378f121dd4f69cdb795cf2883252e9
Prashant Kumar <programmerkrprashant@gmail.com>
2023-12-17 22:02:24 +0530

    rating logic

--- my-amazon-clone/src/components/Stars/Stars.jsx
+++ my-amazon-clone/src/components/Stars/Stars.jsx
@@ -7,20 +7,22 @@ const Stars = ({ stars, reviews }) => {
     const renderStar = () => {
 
         const fullStar = Math.floor(stars)
-        const halfStar = stars - fullStar
-        const renameigStars = 5 - fullStar - (halfStar > 0 ? 1 : 0)
-    
+        const desimalPoint = (stars - fullStar).toFixed(1)
+        const remainingStars = 5 - fullStar - (desimalPoint >= 0.5 ? 1 : 0)
+
         const starCon = []
-        // rendring full star
+        // Render full stars
         for (let i = 0; i < fullStar; i++) {
+
             starCon.push(<i key={i} className="ri-star-fill" style={{ color: '#F3A847' }}></i>)
+
         }
-        // rendring half star
-        if (halfStar > 0 && halfStar < 1) {
+        // Render half star if applicable
+        if (desimalPoint >= 0.5) {
             starCon.push(<i key='key' className="ri-star-half-line" style={{ color: '#F3A847' }}></i>)
         }
-        // rendring empty star
-        for (let index = 0; index < renameigStars; index++) {
+        // Render empty stars for the remaining
+        for (let index = 0; index < remainingStars; index++) {
             starCon.push(<i key={`empty-${index}`} className="ri-star-line" style={{ color: '#F3A847' }}></i>)
         }
 
