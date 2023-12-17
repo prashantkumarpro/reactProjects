88e65c803fba38419488b5dfa50ae18e526542f6
Prashant Kumar <programmerkrprashant@gmail.com>
2023-12-17 15:13:15 +0530

    Ratings and reviews

--- my-amazon-clone/src/components/Stars/Stars.jsx
+++ my-amazon-clone/src/components/Stars/Stars.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 
+
 const Stars = ({ stars, reviews }) => {
 
 
     const renderStar = () => {
 
-        const fullStar = Math.round(stars)
-        const halfStar = 5 - stars
-        console.log(`fullStar: ${fullStar}, halfStar: ${halfStar}  star: ${stars}` )
+        const fullStar = Math.floor(stars)
+        const halfStar = stars - fullStar
+        const renameigStars = 5 - fullStar - (halfStar > 0 ? 1 : 0)
+    
         const starCon = []
         // rendring full star
         for (let i = 0; i < fullStar; i++) {
-            starCon.push()
+            starCon.push(<i key={i} className="ri-star-fill" style={{ color: '#F3A847' }}></i>)
+        }
+        // rendring half star
+        if (halfStar > 0 && halfStar < 1) {
+            starCon.push(<i key='key' className="ri-star-half-line" style={{ color: '#F3A847' }}></i>)
+        }
+        // rendring empty star
+        for (let index = 0; index < renameigStars; index++) {
+            starCon.push(<i key={`empty-${index}`} className="ri-star-line" style={{ color: '#F3A847' }}></i>)
         }
 
 
         return starCon
     }
-    renderStar()
+
 
     return (
         <>
-            {renderStar()}
-            {/* <h2>Half Stars: <i className="ri-star-half-line" style={{}}></i><i className="ri-star-half-line" style={{}}></i> </h2>
-            <h2>Empty Stars:    <i className="ri-star-line" style={{}}></i>    <i className="ri-star-line" style={{}}></i>    <i className="ri-star-line"></i> </h2> */}
+            {stars} {renderStar()} <span style={{ color: '#186592', fontWeight: '500' }}> {reviews} ratings</span>
+            <br />
 
         </>
     )
